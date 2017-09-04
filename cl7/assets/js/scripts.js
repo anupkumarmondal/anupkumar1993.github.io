4d8e5b8fd54a2e8eed052b4a12cdc8e57fcd7372
anupkumarmondal <31000503+anupkumarmondal@users.noreply.github.com>
2017-09-04 16:44:57 +0600

    cl7

---
+++ cl7/assets/js/scripts.js
@@ -0,0 +1,67 @@
+jQuery(document).ready(function($) {
+	$(".homepage-slides").owlCarousel({
+		items: 1,
+		loop: true,
+		nav: true,
+		dots: true,
+		navText: ["<i class='fa fa-long-arrow-left'></i>", "<i class='fa fa-long-arrow-right'></i>"],
+		autoplay: true,
+		autoplayTimeout:3000,
+	});
+
+	$(".team-list").owlCarousel({
+		items: 3,
+		margin: 30,
+		loop: true,
+		nav: false,
+		dots: true,
+		autoplay: true,
+		autoplayTimeout:2000,
+	});
+
+	$(".testimonial-list").owlCarousel({
+		items: 3,
+		margin: 30,
+		loop: true,
+		nav: false,
+		dots: true,
+		autoplay: false,
+	});
+
+	$(".logo-carousel").owlCarousel({
+		items: 5,
+		margin: 30,
+		loop: true,
+		nav: false,
+		dots: false,
+		autoplay: true,
+	});
+
+
+
+	$("#web-design-skillbar").circleProgress({
+		value: 0.9,
+		size: 270,
+		thickness:3,
+		fill:'#22babf',
+	}).on('circle-animation-progress', function(event, progress) {
+	    $(this).find('.skill-count-no').html(Math.round(90 * progress) + '<i>%</i>');
+	});
+	$("#graphic-design-skillbar").circleProgress({
+		value: 0.95,
+		size: 270,
+		thickness:3,
+		fill:'#22babf',
+	}).on('circle-animation-progress', function(event, progress) {
+	    $(this).find('.skill-count-no').html(Math.round(95 * progress) + '<i>%</i>');
+	});
+	$("#digital-marketing-skillbar").circleProgress({
+		value: 0.84,
+		size: 270,
+		thickness:3,
+		fill:'#22babf',
+	}).on('circle-animation-progress', function(event, progress) {
+	    $(this).find('.skill-count-no').html(Math.round(84 * progress) + '<i>%</i>');
+	});
+});
+
