c6bc583c9f2f9adcff914ccc6165ced3bbd96fa4
anupkumarmondal <31000503+anupkumarmondal@users.noreply.github.com>
2017-10-19 22:35:38 +0600

    test

---
+++ testmp/assets/js/scripts.js
@@ -0,0 +1,21 @@
+(function ($) {
+	"use strict";
+
+	jQuery(document).ready(function ($){
+		$(".map").gmap3({
+			address:"Dhaka, Bangladesh",
+			zoom: 16,
+			mapTypeId : google.maps.MapTypeId.ROADMAP
+		});
+
+
+
+
+	});
+
+	jquery(window).load(function (){
+
+	});
+
+	}(jQurey));
+
